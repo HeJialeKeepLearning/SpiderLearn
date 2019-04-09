3a9751c23a6e5cde67c88ab1c051cc90e9bef608
hejiale <hejiale@pku.edu.cn>
2019-04-09 22:54:59 +0800

    修复路径改变导致的import失效问题

--- spiderL1/spiderL1/spiders/ttmeiju.py
+++ spiderL1/spiderL1/spiders/ttmeiju.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
 import scrapy
-from spiderL1.items import Spiderl1Item
-
+from spiderL1.spiderL1.items import Spiderl1Item
 
 class TtmeijuSpider(scrapy.Spider):
-    name = 'ttmeiju'#让scrapy框架定位爬虫的名称，必须唯一
+    name = 'ttmeiju'  # 让scrapy框架定位爬虫的名称，必须唯一
     allowed_domains = ['meijutt.com']
     start_urls = ['https://www.meijutt.com/new100.html']
 
     def parse(self, response):
-        movies=response.xpath('//ul[@class="top-list  fn-clear"]/li')
+        movies = response.xpath('//ul[@class="top-list  fn-clear"]/li')
         for movie in movies:
-            item=Spiderl1Item()
-            item['name']=movie.xpath('./h5/a/@title').extract()[0]
+            item = Spiderl1Item()
+            item['name'] = movie.xpath('./h5/a/@title').extract()[0]
             yield item
