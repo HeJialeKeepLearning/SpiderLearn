5bf3fe940e753a40479dc753b0d9ea3173bf5e2a
HeJialeKeepLearning <hejiale@pku.edu.cn>
2019-04-20 11:13:01 +0800

    update item file

--- maoyanMovie/maoyanMovie/items.py
+++ maoyanMovie/maoyanMovie/items.py
@@ -22,5 +22,15 @@ class movieItem(scrapy.Item):
     poster = scrapy.Field()  # 海报
     rate = scrapy.Field()  # 评分
     id = scrapy.Field()  # 电影id
-    reviewNum = scrapy.Field()  # 评论人数
+    rateNum = scrapy.Field()  # 评分人数
     boxOffice = scrapy.Field()  # 电影票房
+
+class boxofficeItem(scrapy.Item):
+    avgSeatView = scrapy.Field()  # 上座率
+    avgShowView = scrapy.Field()  # 场均人次
+    boxInfo = scrapy.Field()  # 综合票房（万）
+    boxRate = scrapy.Field()  # 票房占比
+    movieId = scrapy.Field()  # 电影id
+    movieName = scrapy.Field()  # 电影名
+    sumBoxInfo = scrapy.Field()  # 总票房
+    showRate = scrapy.Field()  # 排片占比
